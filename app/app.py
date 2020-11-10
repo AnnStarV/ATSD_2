bae42360cd8ba598f91ab374c43011604e268d9e
AnnStarV <anastarsova8@gmail.com>
2020-11-10 08:26:59 +0300

    created second class

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-# import json
-# from random import randint
+import json
+from random import randint
+
+
 #
 # class Node:
 #     def __init__(self, value = None, next = None):
@@ -107,3 +109,154 @@
 # N.delete(92)
 # N.__str__()
 # N.find("Панфилов")
+
+# class BTreeNode:
+#     def __init__(self, leaf=False):
+#         self.leaf = leaf
+#         self.keys = []
+#         self.child = []
+#
+# class BTree:
+#     def __init__(self, t):
+#         self.root = BTreeNode(True)
+#         self.t = t
+#
+#     # Print the tree
+#     def print_tree(self, x, l=0):
+#         print("Level ", l, " ", len(x.keys), end=":")
+#         for i in x.keys:
+#             print(i, end=" ")
+#         print()
+#         l += 1
+#         if len(x.child) > 0:
+#             for i in x.child:
+#                 self.print_tree(i, l)
+#
+#     # Search 1
+#     def search_by_date(self, k, x=None):
+#         if x is not None:
+#             i = 0
+#             while i < len(x.keys) and k > x.keys[i]['date']:
+#                 i += 1
+#             if i < len(x.keys) and k == x.keys[i]['date']:
+#                 print("find:", x.keys[i], '\n')
+#                 return
+#             elif x.leaf:
+#                 print("find: Not exists!\n")
+#                 return None
+#             else:
+#                 return self.search_by_date(k, x.child[i])
+#         else:
+#             return self.search_by_date(k, self.root)
+#
+#     def top_rain_dom(self, k, x=None):
+#         arr = []
+#         self.top_rain_stor(arr, k, self.root)
+#         max = arr[0]
+#         for i in range(len(arr)):
+#             if(int(max['rain'].replace('%', '')) < int(arr[i]['rain'].replace('%', ''))):
+#                 max = arr[i]
+#         print("top rain: ", max, '\n')
+#
+#     # Метод для поиска всех дат из заданого месяца
+#     def top_rain_stor(self, arr, k, x):
+#         for i in x.keys:
+#             if(i['date'].find(k) != - 1):
+#                 arr.append(i)
+#         if (len(x.child) > 0):
+#             for i in x.child:
+#                 self.top_rain_stor(arr, k, i)
+#     # Search 2
+#
+#     # Insert the key
+#
+#     def insert_key(self, k):
+#         root = self.root
+#         if len(root.keys) == (2 * self.t) - 1:
+#             temp = BTreeNode()
+#             self.root = temp
+#             temp.child.insert(0, root)
+#             self.split(temp, 0)
+#             self.insert_non_full(temp, k)
+#         else:
+#             self.insert_non_full(root, k)
+#
+#     # Insert non full condition
+#     def insert_non_full(self, x, k):
+#         i = len(x.keys) - 1
+#         if x.leaf:
+#             x.keys.append(None)
+#             while i >= 0 and k['date'] < x.keys[i]['date']:
+#                 x.keys[i + 1] = x.keys[i]
+#                 i -= 1
+#             x.keys[i + 1] = k
+#         else:
+#             while i >= 0 and k['date'] < x.keys[i]['date']:
+#                 i -= 1
+#             i += 1
+#             if len(x.child[i].keys) == (2 * self.t) - 1:
+#                 self.split(x, i)
+#                 if k['date'] > x.keys[i]['date']:
+#                     i += 1
+#             self.insert_non_full(x.child[i], k)
+#     # Split
+#
+#     def split(self, x, i):
+#         t = self.t
+#         y = x.child[i]
+#         z = BTreeNode(y.leaf)
+#         x.child.append(z)
+#         x.keys.append(y.keys[t - 1])
+#         z.keys = y.keys[t: (2 * t) - 1]
+#         y.keys = y.keys[0: t - 1]
+#         if not y.leaf:
+#             z.child = y.child[t: 2 * t]
+#             y.child = y.child[0: t - 1]
+#     # delete elem from Tree
+#
+#     def remove(self, x):
+#         F = BTree(self.t)
+#         arr = []
+#         self.createStorage(self.root, arr)
+#
+#         for i in range(len(arr)):
+#             if(x == arr[i]['date']):
+#                 arr.remove(arr[i])
+#                 for i in range(len(arr)):
+#                     F.insert_key(arr[i])
+#                 self.root = F.root
+#                 return
+#         print('element isn\'t found ')
+#     # Create an array storing all the elements of the tree
+#
+#     def createStorage(self, x, arr):
+#         for i in x.keys:
+#             arr.append(i)
+#             # if(not i[1] in arr):
+#         if len(x.child) > 0:
+#             for i in x.child:
+#                 self.createStorage(i, arr)
+#
+#
+#
+#
+#
+# B = BTree(3)
+# with open("data.json", "r", encoding='utf-8') as read_file:
+#     data = json.load(read_file)
+# list_data = data["weather"]
+# for i in list_data:
+#     B.insert_key(i)
+# # Вывод деревка
+# B.print_tree(B.root)
+# # Поиск по мобилке
+# B.search_by_date("22/10/2020")
+# # Поиск в заданом месяце(год уточняется во избежание тупиковых ситуаций) дня с самыми большими осадками
+# B.top_rain_dom("10/2020")
+# # удаление
+# B.remove("21/11/2020")
+# B.remove("21/11/2020")
+# print()
+# B.print_tree(B.root)
+#
+# # B.print_tree(B.root)
